6501061037e9b0cb2f78c24dc8327a85d2214b5d
Shamal Faily <shamal.faily@googlemail.com>
2012-06-28 23:19:53 +0100

    Changed format parameters to type

--- cairis/cairis/cexport.py
+++ cairis/cairis/cexport.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
   try:
     parser = argparse.ArgumentParser(description='Computer Aided Integration of Requirements and Information Security - Model Export to Redmine')
     parser.add_argument('outputFile',help='output file name')
-    parser.add_argument('--export',dest='modelFormat',help='model type to export.  One of requirements, scenarios, usecases, or GRL')
+    parser.add_argument('--type',dest='modelFormat',help='model type to export.  One of requirements, scenarios, usecases, or GRL')
     parser.add_argument('--persona',dest='personaName',help='Persona name (relevant for GRL export only)')
     parser.add_argument('--task',dest='taskName',help='Task name (relevant for GRL export only)')
     parser.add_argument('--environment',dest='envName',help='Environment name (relevant for GRL export only)')
