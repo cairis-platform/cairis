a1091092ced0e121ee54c60d803dc314bb5d0e61
Shamal Faily <shamal.faily@googlemail.com>
2015-12-02 08:11:50 +0000

    DB creation and deletion scripts for test cases

---
+++ cairis/cairis/test/createdb.sql
@@ -0,0 +1,25 @@
+/* 
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*/
+
+grant usage on *.* to 'irisuser'@'localhost' identified by '' with max_queries_per_hour 0 max_connections_per_hour 0 max_updates_per_hour 0 max_user_connections 0;
+create database if not exists `arm`;
+grant all privileges on `arm`.* to 'irisuser'@'localhost';
+set global max_sp_recursion_depth = 255;
+flush tables;
+flush privileges;
