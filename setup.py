632a8d1b6bf01edec5a96d7d1fe978ddde4828c9
Shamal Faily <shamal.faily@googlemail.com>
2015-12-03 00:23:32 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name = 'cairis',
+      version = '1.0',
+      description='Computer Aided Integration of Requirements and Information Security',
+      author = 'Shamal Faily',
+      author_email = 'shamal.faily@googlemail.com',
+      url = 'http://cairis.org',
+      packages=['cairis'],
+      package_data={'cairis':['cairis']}
+     )
