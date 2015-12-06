3cac2a80981331b1702114e58e02657a9e33db6e
Shamal Faily <shamal.faily@googlemail.com>
2015-12-06 09:00:26 +0000

    Tweak environment tests

--- cairis/cairis/test/test_Environment.py
+++ cairis/cairis/test/test_Environment.py
@@ -21,25 +21,45 @@ import json
 import BorgFactory
 from Borg import Borg
 from EnvironmentParameters import EnvironmentParameters
+from ARM import DatabaseProxyException
 
 class EnvironmentTest(unittest.TestCase):
 
   def setUp(self):
     os.system("$CAIRIS_SRC/test/initdb.sh")
     BorgFactory.initialise()
+    f = open(os.environ['CAIRIS_SRC'] + '/test/environments.json')
+    d = json.load(f)
+    f.close()
+    self.ienvs = d['environments']
+    
 
   def testStandardEnvironment(self):
-    f = open(os.environ['CAIRIS_SRC'] + '/test/testStandardEnvironment.json')
-    d = json.load(f)
-    iep = EnvironmentParameters(d["theName"],d["theShortCode"],d["theDescription"])
+    iep1 = EnvironmentParameters(self.ienvs[0]["theName"],self.ienvs[0]["theShortCode"],self.ienvs[0]["theDescription"])
+    iep2 = EnvironmentParameters(self.ienvs[1]["theName"],self.ienvs[1]["theShortCode"],self.ienvs[1]["theDescription"])
+    iep3 = EnvironmentParameters(self.ienvs[2]["theName"],self.ienvs[2]["theShortCode"],self.ienvs[2]["theDescription"])
     b = Borg()
-    b.dbProxy.addEnvironment(iep)
-    envs = b.dbProxy.getEnvironments()
-    oep = envs[d["theName"]]
-    self.assertEqual(iep.name(),oep.name())
-    self.assertEqual(iep.shortCode(),oep.shortCode())
-    self.assertEqual(iep.description(),oep.description())
-    f.close()
+    b.dbProxy.addEnvironment(iep1)
+    b.dbProxy.addEnvironment(iep2)
+    b.dbProxy.addEnvironment(iep3)
+    oenvs = b.dbProxy.getEnvironments()
+    oep1 = oenvs[self.ienvs[0]["theName"]]
+    self.assertEqual(iep1.name(), oep1.name())
+    self.assertEqual(iep1.shortCode(),oep1.shortCode())
+    self.assertEqual(iep1.description(),oep1.description())
+    oep2 = oenvs[self.ienvs[1]["theName"]]
+    self.assertEqual(iep2.name(), oep2.name())
+    self.assertEqual(iep2.shortCode(),oep2.shortCode())
+    self.assertEqual(iep2.description(),oep2.description())
+    oep3 = oenvs[self.ienvs[2]["theName"]]
+    self.assertEqual(iep3.name(), oep3.name())
+    self.assertEqual(iep3.shortCode(),oep3.shortCode())
+    self.assertEqual(iep3.description(),oep3.description())
+
+    b.dbProxy.deleteEnvironment(oep1.id())
+    b.dbProxy.deleteEnvironment(oep2.id())
+    b.dbProxy.deleteEnvironment(oep3.id())
+  
 
   def tearDown(self):
     b = Borg()
