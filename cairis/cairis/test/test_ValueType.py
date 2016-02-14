4b17fd6ed8df89d7b4d7c47e3df9bf1c3eab918f
ash-ley <i7624156@bournemouth.ac.uk>
2016-02-14 22:12:20 +0000

    Update test_ValueType.py

--- cairis/cairis/test/test_ValueType.py
+++ cairis/cairis/test/test_ValueType.py
@@ -21,6 +21,7 @@ import json
 import BorgFactory
 from Borg import Borg
 from ValueTypeParameters import ValueTypeParameters
+from ARM import DatabaseProxyException
 
 class ValueTypeTest(unittest.TestCase):
 
@@ -31,22 +32,21 @@ class ValueTypeTest(unittest.TestCase):
     f.close()
     self.iVtypes = d['types']
     
-
   def testValueType(self):
-    ivt1 = ValueTypeParameters(self.iVtypes[0]["theName"], self.iVtypes[0]["theDescription"], self.iVtypes[0]["theType"])
-    ivt2 = ValueTypeParameters(self.iVtypes[1]["theName"], self.iVtypes[1]["theDescription"], self.iVtypes[1]["theType"])
+    ivt1 = ValueTypeParameters(self.iVtypes[0]["theName"], self.iVtypes[0]["theDescription"], self.iVtypes[0]["theType"], [], [], [])
+    ivt2 = ValueTypeParameters(self.iVtypes[1]["theName"], self.iVtypes[1]["theDescription"], self.iVtypes[1]["theType"], [], [], [])
     b = Borg()
     b.dbProxy.addValueType(ivt1)
     b.dbProxy.addValueType(ivt2)
     oVtypes = b.dbProxy.getValueTypes()
     ovt1 = oVtypes[self.iVtypes[0]["theName"]]
     self.assertEqual(ivt1.name(), ovt1.name())
-    self.assertEqual(ivt1.shortCode(),ovt1.description())
-    self.assertEqual(ivt1.description(),ovt1.type())
+    self.assertEqual(ivt1.description(),ovt1.description())
+    self.assertEqual(ivt1.type(),ovt1.type())
     ovt2 = oVtypes[self.iVtypes[1]["theName"]]
     self.assertEqual(ivt2.name(), ovt2.name())
-    self.assertEqual(ivt2.shortCode(),ovt2.description())
-    self.assertEqual(ivt2.description(),ovt2.type())
+    self.assertEqual(ivt2.description(),ovt2.description())
+    self.assertEqual(ivt2.type(),ovt2.type())
 
     b.dbProxy.deleteVulnerabilityType(ovt1.id())
     b.dbProxy.deleteThreatType(ovt2.id())
