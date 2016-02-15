5bae294c3c07686ca5a74098fa8587c00e5a23b3
ash-ley <i7624156@bournemouth.ac.uk>
2016-02-15 19:54:31 +0000

    Update test_ValueType.py

--- cairis/cairis/test/test_ValueType.py
+++ cairis/cairis/test/test_ValueType.py
@@ -37,13 +37,12 @@ class ValueTypeTest(unittest.TestCase):
     b = Borg()
     b.dbProxy.addValueType(ivt1)
     b.dbProxy.addValueType(ivt2)
-    oVvtypes = b.dbProxy.getValueTypes('vulnerability_type')
-    ovt1 = oVvtypes[self.iVtypes[0]["theName"]]
+    oVtypes = b.dbProxy.getValueTypes()
+    ovt1 = oVtypes[self.iVtypes[0]["theName"]]
     self.assertEqual(ivt1.name(), ovt1.name())
     self.assertEqual(ivt1.description(),ovt1.description())
     self.assertEqual(ivt1.type(),ovt1.type())
-    oVttypes = b.dbProxy.getValueTypes('threat_type')
-    ovt2 = oVttypes[self.iVtypes[1]["theName"]]
+    ovt2 = oVtypes[self.iVtypes[1]["theName"]]
     self.assertEqual(ivt2.name(), ovt2.name())
     self.assertEqual(ivt2.description(),ovt2.description())
     self.assertEqual(ivt2.type(),ovt2.type())
