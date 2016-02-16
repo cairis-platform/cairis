c714ace14c547c109895be212bea80990bfea0b9
ash-ley <i7624156@bournemouth.ac.uk>
2016-02-16 13:00:35 +0000

    Update test_ValueType.py

--- cairis/cairis/test/test_ValueType.py
+++ cairis/cairis/test/test_ValueType.py
@@ -38,7 +38,7 @@ class ValueTypeTest(unittest.TestCase):
     b.dbProxy.addValueType(ivt1)
     b.dbProxy.addValueType(ivt2)
     oVVtypes = b.dbProxy.getValueTypes('vulnerability_type')
-    ovt1 = oVVtypes[values["typeName"]]
+    ovt1 = oVVtypes[0]
     self.assertEqual(ivt1.name(), ovt1.name())
     self.assertEqual(ivt1.description(),ovt1.description())
     self.assertEqual(ivt1.type(),ovt1.type())
