a8dae435c3bef1e1f3a4c81f4ffffab8cb5693c8
ash-ley <i7624156@bournemouth.ac.uk>
2016-03-07 21:15:17 +0000

    Update test_Attacker.py

--- cairis/cairis/test/test_Attacker.py
+++ cairis/cairis/test/test_Attacker.py
@@ -92,8 +92,8 @@ class AttackerTest(unittest.TestCase):
     
     b.dbProxy.deleteDocumentReference(self.odrs[self.idr1.name()].id())
     b.dbProxy.deleteDocumentReference(self.odrs[self.idr2.name()].id())
-    b.dbProxy.deleteExternalDocument(self.oec[self.iec1.name()].id())
-    b.dbProxy.deleteExternalDocument(self.oec[self.iec2.name()].id())
+    b.dbProxy.deleteExternalDocument(self.oecs[self.iec1.name()].id())
+    b.dbProxy.deleteExternalDocument(self.oecs[self.iec2.name()].id())
     b.dbProxy.deletePersona(self.opp[self.ipp.name()].id())
     b.dbProxy.deleteRole(self.oRoles[self.irp.name()].id())
     b.dbProxy.deleteEnvironment(self.oenvs[self.iep.name()].id())
