4bf08f0b92ba7d1d85475ac78ad80c0cb2217826
ash-ley <i7624156@bournemouth.ac.uk>
2016-03-13 20:31:20 +0000

    Update test_Threat.py

--- cairis/cairis/test/test_Threat.py
+++ cairis/cairis/test/test_Threat.py
@@ -103,7 +103,7 @@ class ThreatTest(unittest.TestCase):
     self.assertEqual(iteps[0].likelihood()[0], oteps[0].likelihood()[0])
     self.assertEqual(str(iteps[0].assets()[0]), str(oteps[0].assets()[0]))
     self.assertEqual(str(iteps[0].attackers()[0]), str(oteps[0].attackers()[0]))
-    self.assertEqual(str(iteps[0].properties()[1]), str(oteps[0].properties()[1]))
+    self.assertEqual(str(iteps[0].properties()[0]), str(oteps[0].properties()[0][0]))
     self.assertEqual(str(iteps[0].properties()[2]), str(oteps[0].properties()[2]))
 
     b.dbProxy.deleteThreat(o.id())
