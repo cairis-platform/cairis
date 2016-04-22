c521fc8dae591da835c60f32577755ce3b70e096
cairis <cairis@debian>
2016-04-22 12:44:14 +0100

    Configure CAIRIS script

---
+++ cairis/bin/configure_cairis_db.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
+import npyscreen as np
+import os
+import MySQLdb
+import _mysql_exceptions
+from subprocess import Popen
+
+class CAIRISConfigurationForm(np.ActionForm):
+
+  def create(self):
+    self.name = "Configure CAIRIS database"
+    self.theHost = self.add(np.TitleText, name = "Host:", value = "localhost")
+    self.thePort = self.add(np.TitleText, name = "Port:", value = "3306")
+    self.theUser = self.add(np.TitleText, name = "User:", value = "cairisuser")
+    self.theRootPassword = self.add(np.TitlePassword, name = "Database root password:", value = "")
+    self.thePassword = self.add(np.TitlePassword, name = "User Password:", value = "cairis123")
+    self.theDbName = self.add(np.TitleText, name = "Database name:", value = "cairis")
+    self.theTmpDir = self.add(np.TitleText, name = "Temp directory:", value = "/tmp")
+    self.theRootDir = self.add(np.TitleText, name = "Root directory:", value = "/usr/local/lib/python2.7/dist-packages/cairis")
+    self.theImageDir = self.add(np.TitleText, name = "Default image directory:", value = ".")
+    self.theFileName = self.add(np.TitleText, name = "CAIRIS configuration file name:", value = os.environ.get("HOME") + "/cairis.cnf")
+
+  def on_ok(self):
+    self.createDatabase()
+    self.initialiseDatabase()
+    self.createCairisCnf()
+
+  def on_cancel(self):
+    self.parentApp.setNextForm(None)
+
+  def createDatabase(self):
+    rootConn = MySQLdb.connect(host=self.theHost.value,port=int(self.thePort.value),user='root',passwd=self.theRootPassword.value)
+    rootCursor = rootConn.cursor()
+
+    try:
+      grantUsageSql = "grant usage on *.* to '" + self.theUser.value + "'@'" + self.theHost.value + "' identified by '" + self.thePassword.value + "' with max_queries_per_hour 0 max_connections_per_hour 0 max_updates_per_hour 0 max_user_connections 0"
+      rootCursor.execute(grantUsageSql)
+    except _mysql_exceptions.DatabaseError, e:
+      id,msg = e
+      exceptionText = 'MySQL error granting usage to ' + self.theUser.value + ' (id: ' + str(id) + ', message: ' + msg
+
+    try:
+      createSql = "create database if not exists `" + self.theDbName.value + "`"
+      rootCursor.execute(createSql)
+    except _mysql_exceptions.DatabaseError, e:
+      id,msg = e
+      exceptionText = 'MySQL error creating ' + self.theDbName.value + ' database (id: ' + str(id) + ', message: ' + msg
+
+    try:
+      grantPrivilegesSql = "grant all privileges on `" + self.theDbName.value + "`.* to '" + self.theUser.value + "'@'" + self.theHost.value + "'"
+      rootCursor.execute(grantPrivilegesSql)
+    except _mysql_exceptions.DatabaseError, e:
+      id,msg = e
+      exceptionText = 'MySQL error granting privileges to ' + self.theUser.value + ' for ' + self.theDbName.value + ' database (id: ' + str(id) + ', message: ' + msg
+
+    try:
+      recursionDepthSql = "set global max_sp_recursion_depth = 255"
+      rootCursor.execute(recursionDepthSql)
+    except _mysql_exceptions.DatabaseError, e:
+      id,msg = e
+      exceptionText = 'MySQL error setting recursion depth ' + self.theUser.value + ' for ' + self.theDbName.value + ' database (id: ' + str(id) + ', message: ' + msg
+
+    try:
+      flushPrivilegesSql = "flush privileges"
+      rootCursor.execute(flushPrivilegesSql)
+    except _mysql_exceptions.DatabaseError, e:
+      id,msg = e
+      exceptionText = 'MySQL error flushing privileges (id: ' + str(id) + ', message: ' + msg
+
+    rootCursor.close()
+    rootConn.close()
+
+  def initialiseDatabase(self):
+    initDbCmd = "mysql --user=" + self.theUser.value + " --password=" + self.thePassword.value + " --database=" + self.theDbName.value + " < " + self.theRootDir.value + "/sql/init.sql"
+    p = Popen(initDbCmd,shell=True)
+    os.waitpid(p.pid,0)
+
+    procsCmd = "mysql --user=" + self.theUser.value + " --password=" + self.thePassword.value + " --database=" + self.theDbName.value + " < " + self.theRootDir.value + "/sql/procs.sql"
+    p = Popen(procsCmd,shell=True)
+    os.waitpid(p.pid,0)
+
+
+  def createCairisCnf(self):
+    f = open(self.theFileName.value,'w')
+    f.write("dbhost = " + self.theHost.value + "\n")
+    f.write("dbport = " + self.thePort.value + "\n")
+    f.write("dbuser = " + self.theUser.value + "\n")
+    f.write("dbpasswd = " + self.thePassword.value + "\n")
+    f.write("dbname = " + self.theDbName.value + "\n")
+    f.write("tmp_dir = " + self.theTmpDir.value + "\n")
+    f.write("root = " + self.theRootDir.value + "\n")
+    f.write("default_image_dir = " + self.theImageDir.value + "\n")
+    f.close()
+    self.parentApp.setNextForm(None)
+
+  
+
+class CAIRISConfigurationApp(np.NPSAppManaged):
+  def onStart(self):
+    self.addForm("MAIN",CAIRISConfigurationForm)
+
+if __name__ == '__main__':
+  App = CAIRISConfigurationApp()
+  App.run() 
