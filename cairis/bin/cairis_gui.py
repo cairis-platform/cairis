784ac28a02059765cfbae9eada7c98ac13d1b18a
Shamal Faily <shamal.faily@googlemail.com>
2016-04-24 10:39:39 +0100

    Other reasons for ImportError - especially on other OSs

--- cairis/bin/cairis_gui.py
+++ cairis/bin/cairis_gui.py
@@ -32,8 +32,8 @@ if __name__ == '__main__':
     from cairis.gui.IRISApp import IRISApp
     from cairis.core.ARM import ARMException
     main()
-  except ImportError:
-    print "Fatal CAIRIS error: Could not import the Python dependencies needed by CAIRIS.  Either your Python installation is incomplete, or - if you have downloaded CAIRIS directly from github - PYTHONPATH needs to be set to the root directly of your source installation; this is the same directory that setup.py can be found in."
+  except ImportError, e:
+    print "Fatal CAIRIS error: " + str(e) 
     sys.exit(-1)
   except ARMException, e:
     print "Fatal CAIRIS error: " + str(e) 
