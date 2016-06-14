9e641411c0a65a0e48351ba989d899a9b0436500
Shamal Faily <shamal.faily@googlemail.com>
2016-06-15 00:35:59 +0100

    Missing tests

---
+++ cairis/test/test_ComponentView.py
@@ -0,0 +1,136 @@
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
+import unittest
+import os
+import json
+from subprocess import call
+import cairis.core.BorgFactory
+from cairis.core.Borg import Borg
+from cairis.core.RoleParameters import RoleParameters
+from cairis.core.TemplateAssetParameters import TemplateAssetParameters
+from cairis.core.TemplateGoalParameters import TemplateGoalParameters
+from cairis.core.ValueTypeParameters import ValueTypeParameters
+from cairis.core.ComponentParameters import ComponentParameters
+from cairis.core.ConnectorParameters import ConnectorParameters
+from cairis.core.ComponentViewParameters import ComponentViewParameters
+
+class ComponentViewTest(unittest.TestCase):
+
+  def setUp(self):
+    call([os.environ['CAIRIS_SRC'] + "/test/initdb.sh"])
+    cairis.core.BorgFactory.initialise()
+    f = open(os.environ['CAIRIS_SRC'] + '/test/componentviews.json')
+    d = json.load(f)
+    f.close()
+
+    self.theRequirements = []
+    self.theRoles = []
+    self.iRoles = d['roles']
+    for i in self.iRoles:
+      self.theRoles.append(RoleParameters(i["theName"], i["theType"], i["theShortCode"], i["theDescription"],[]))
+
+    self.theMetricTypes = []
+    self.iAccessRights = d['access_rights']
+    for i in self.iAccessRights:
+      self.theMetricTypes.append(ValueTypeParameters(i["theName"], i["theDescription"], 'access_right','',i["theValue"],i["theRationale"]))
+
+    self.iSurfaceTypes = d['surface_types']
+    for i in self.iSurfaceTypes:
+      self.theMetricTypes.append(ValueTypeParameters(i["theName"], i["theDescription"], 'surface_type','',i["theValue"],i["theRationale"]))
+
+    self.iProtocols = d['protocols']
+    for i in self.iProtocols:
+      self.theMetricTypes.append(ValueTypeParameters(i["theName"], i["theDescription"], 'protocol','',i["theValue"],i["theRationale"]))
+
+    self.iPrivileges = d['privileges']
+    for i in self.iPrivileges:
+      self.theMetricTypes.append(ValueTypeParameters(i["theName"], i["theDescription"], 'privilege','',i["theValue"],i["theRationale"]))
+
+    self.theAssets = []
+    spValues = [(0,'None'),(0,'None'),(0,'None'),(0,'None'),(0,'None'),(0,'None'),(0,'None'),(0,'None')]
+    self.iTemplateAssets = d['template_assets']
+    for i in self.iTemplateAssets:
+      self.theAssets.append(TemplateAssetParameters(i["theName"], i["theShortCode"], i["theDescription"], i["theSignificance"],i["theType"],i["theSurfaceType"],i["theAccessRight"],spValues,[],[]))
+
+    self.theGoals = []
+    self.iTemplateGoals = d['template_goals']
+    for i in self.iTemplateGoals:
+      self.theGoals.append(TemplateGoalParameters(i["theName"],i["theDefinition"],i["theRationale"],i["theConcerns"],i["theResponsibilities"]))
+
+    self.iComponentViews = d['component_views']
+    
+
+  def testComponentView(self):
+
+    cvName = self.iComponentViews[0]["theName"]
+    cvSyn = self.iComponentViews[0]["theSynopsis"]
+    theComponents = []
+    for c in self.iComponentViews[0]["theComponents"]:
+      cName = c["theName"]
+      cDesc = c["theDescription"]
+      cInts = []
+      for i in c["theInterfaces"]:
+        cInts.append((i["theName"],i["theType"],i["theAccessRight"],i["thePrivilege"]))
+      cStructs = []
+      for cs in c["theStructure"]:
+        cStructs.append((cs["theHeadAsset"],cs["theHeadAdornment"],cs["theHeadNav"],cs["theHeadNry"],cs["theHeadRole"],cs["theTailRole"],cs["theTailNry"],cs["theTailNav"],cs["theTailAdornment"],cs["theTailAsset"]))
+      cReqs = []
+      cGoals = []
+      for i in c["theComponentGoals"]:
+        cGoals.append(i)
+
+      cGoalAssocs = []
+      for cga in c["theComponentGoalAssociations"]:
+        cGoalAssocs.append((cga["theGoalName"],cga["theSubGoalName"],cga["theRefType"],'None'))
+      theComponents.append(ComponentParameters(cName,cDesc,cInts,cStructs,cReqs,cGoals,cGoalAssocs))
+
+    theConnectors = []
+    for conn in self.iComponentViews[0]["theConnectors"]:
+      theConnectors.append(ConnectorParameters(conn["theName"],cvName,conn["theFromComponent"],conn["theFromRole"],conn["theFromInterface"],conn["theToComponent"],conn["theToInterface"],conn["theToRole"],conn["theAssetName"],conn["theProtocol"],conn["theAccessRight"]))
+ 
+    icvp = ComponentViewParameters(cvName,cvSyn,self.theMetricTypes,self.theRoles,self.theAssets,self.theRequirements,self.theGoals,theComponents,theConnectors)
+    b = Borg()
+    b.dbProxy.addComponentView(icvp)
+
+    ocvps = b.dbProxy.getComponentViews()
+    ocvp = ocvps[cvName]
+
+    self.assertEqual(icvp.name(), ocvp.name())
+    self.assertEqual(icvp.synopsis(), ocvp.synopsis())
+
+    icvp.setId(ocvp.id())
+    icvp.theSynopsis = 'revised synopsis'
+    b.dbProxy.updateComponentView(icvp)
+    ocvps = b.dbProxy.getComponentViews()
+    ocvp = ocvps[cvName]
+
+    self.assertEqual(icvp.name(), ocvp.name())
+    self.assertEqual(ocvp.synopsis(), 'revised synopsis')
+
+
+
+    b.dbProxy.deleteComponentView(ocvp.id())
+  
+  def tearDown(self):
+    b = Borg()
+
+    b.dbProxy.close()
+    call([os.environ['CAIRIS_SRC'] + "/test/dropdb.sh"])
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ cairis/test/test_TemplateGoal.py
@@ -0,0 +1,87 @@
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
+import unittest
+import os
+import json
+from subprocess import call
+import cairis.core.BorgFactory
+from cairis.core.Borg import Borg
+from cairis.core.TemplateAssetParameters import TemplateAssetParameters
+from cairis.core.TemplateGoalParameters import TemplateGoalParameters
+from cairis.core.ValueTypeParameters import ValueTypeParameters
+
+class TemplateGoalTest(unittest.TestCase):
+
+  def setUp(self):
+    call([os.environ['CAIRIS_SRC'] + "/test/initdb.sh"])
+    cairis.core.BorgFactory.initialise()
+    f = open(os.environ['CAIRIS_SRC'] + '/test/templategoals.json')
+    d = json.load(f)
+    f.close()
+    self.iAccessRights = d['access_rights']
+    iar1 = ValueTypeParameters(self.iAccessRights[0]["theName"], self.iAccessRights[0]["theDescription"], 'access_right','',self.iAccessRights[0]["theValue"],self.iAccessRights[0]["theRationale"])
+    self.iSurfaceTypes = d['surface_type']
+    ist1 = ValueTypeParameters(self.iSurfaceTypes[0]["theName"], self.iSurfaceTypes[0]["theDescription"], 'surface_type','',self.iSurfaceTypes[0]["theValue"],self.iSurfaceTypes[0]["theRationale"])
+    b = Borg()
+    b.dbProxy.addValueType(iar1)
+    b.dbProxy.addValueType(ist1)
+    self.iTemplateAssets = d['template_assets']
+    spValues = [(0,'None'),(0,'None'),(0,'None'),(0,'None'),(0,'None'),(0,'None'),(0,'None'),(0,'None')]
+    iTap = TemplateAssetParameters(self.iTemplateAssets[0]["theName"], self.iTemplateAssets[0]["theShortCode"], self.iTemplateAssets[0]["theDescription"], self.iTemplateAssets[0]["theSignificance"],self.iTemplateAssets[0]["theType"],self.iTemplateAssets[0]["theSurfaceType"],self.iTemplateAssets[0]["theAccessRight"],spValues,[],[])
+    b.dbProxy.addTemplateAsset(iTap)
+    oTaps = b.dbProxy.getTemplateAssets()
+    self.oTap = oTaps[self.iTemplateAssets[0]["theName"]]
+    self.iTemplateGoals = d['template_goals']
+
+  def testTemplateGoal(self):
+    b = Borg()
+    iTag1 = TemplateGoalParameters(self.iTemplateGoals[0]["theName"],self.iTemplateGoals[0]["theDefinition"],self.iTemplateGoals[0]["theRationale"],self.iTemplateGoals[0]["theConcerns"],self.iTemplateGoals[0]["theResponsibilities"])
+    iTag2 = TemplateGoalParameters(self.iTemplateGoals[1]["theName"],self.iTemplateGoals[1]["theDefinition"],self.iTemplateGoals[1]["theRationale"],self.iTemplateGoals[1]["theConcerns"],self.iTemplateGoals[1]["theResponsibilities"])
+    b.dbProxy.addTemplateGoal(iTag1)
+    b.dbProxy.addTemplateGoal(iTag2)
+
+
+    oTags = b.dbProxy.getTemplateGoals()
+    oTag1 = oTags[self.iTemplateGoals[0]["theName"]]
+    oTag2 = oTags[self.iTemplateGoals[1]["theName"]]
+
+
+    self.assertEqual(iTag1.name(), oTag1.name())
+    self.assertEqual(iTag1.definition(), oTag1.definition())
+    self.assertEqual(iTag1.rationale(),oTag1.rationale())
+    self.assertEqual(iTag1.concerns(),oTag1.concerns())
+    self.assertEqual(iTag1.responsibilities(),oTag1.responsibilities())
+
+    self.assertEqual(iTag2.name(), oTag2.name())
+    self.assertEqual(iTag2.definition(), oTag2.definition())
+    self.assertEqual(iTag2.rationale(),oTag2.rationale())
+    self.assertEqual(iTag2.concerns(),oTag2.concerns())
+    self.assertEqual(iTag2.responsibilities(),oTag2.responsibilities())
+
+    b.dbProxy.deleteTemplateGoal(oTag1.id())
+    b.dbProxy.deleteTemplateGoal(oTag2.id())
+  
+  def tearDown(self):
+    b = Borg()
+    b.dbProxy.deleteTemplateAsset(self.oTap.id())
+
+    b.dbProxy.close()
+    call([os.environ['CAIRIS_SRC'] + "/test/dropdb.sh"])
+
+if __name__ == '__main__':
+  unittest.main()
