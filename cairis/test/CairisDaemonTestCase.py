5e23134b74a07f6743087d30817dc511018200e4
Shamal Faily <shamal.faily@googlemail.com>
2016-07-21 20:23:28 +0100

    Take code out of CairisDaemonTestCase constructor

--- cairis/test/CairisDaemonTestCase.py
+++ cairis/test/CairisDaemonTestCase.py
@@ -25,7 +25,6 @@ __author__ = 'Robin Quetin'
 
 
 class CairisDaemonTestCase(unittest.TestCase):
-  def __init__(self):
-    call([os.environ['CAIRIS_SRC'] + "/test/initdb.sh"])
-    app = cairis.bin.cairisd.main(['-d', '--unit-test'])
-    sleep(1)
+  call([os.environ['CAIRIS_SRC'] + "/test/initdb.sh"])
+  app = cairis.bin.cairisd.main(['-d', '--unit-test'])
+  sleep(1)
