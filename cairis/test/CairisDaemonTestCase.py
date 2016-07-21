bb24dbc5d96df868b9fdab46e536de003c044cc8
Shamal Faily <shamal.faily@googlemail.com>
2016-07-21 20:39:49 +0100

    Edits to base class constructor

--- cairis/test/CairisDaemonTestCase.py
+++ cairis/test/CairisDaemonTestCase.py
@@ -25,6 +25,6 @@ __author__ = 'Robin Quetin'
 
 
 class CairisDaemonTestCase(unittest.TestCase):
-  call([os.environ['CAIRIS_SRC'] + "/test/initdb.sh"])
+#  call([os.environ['CAIRIS_SRC'] + "/test/initdb.sh"])
   app = cairis.bin.cairisd.main(['-d', '--unit-test'])
   sleep(1)
