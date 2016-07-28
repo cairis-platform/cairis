ffa23419f2ed2bce60fe81a10d40c941a728abdd
Shamal Faily <shamal.faily@googlemail.com>
2016-07-28 22:20:20 +0100

    Add license  headers

--- cairis/controllers/CImportController.py
+++ cairis/controllers/CImportController.py
@@ -1,14 +1,29 @@
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
 import httplib
 from os import close as fd_close
 from os import remove as remove_file
 from tempfile import mkstemp
 from urllib import unquote
-
 from flask import make_response, request, session
 from flask.ext.restful import Resource
 from flask_restful_swagger import swagger
 from werkzeug.datastructures import FileStorage
-
 from cairis.core.ARM import DatabaseProxyException, ARMException
 from cairis.core.Borg import Borg
 from cairis.daemon.CairisHTTPError import MalformedJSONHTTPError, CairisHTTPError, ARMHTTPError, MissingParameterHTTPError
