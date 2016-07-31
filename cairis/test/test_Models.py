9be8d1218e2efcb6673dd2303e8c3967955d7cff
Shamal Faily <shamal.faily@googlemail.com>
2016-07-31 09:17:52 +0100

    Test model creation

---
+++ cairis/test/test_Models.py
@@ -0,0 +1,42 @@
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
+import unittest
+import os
+from cairis.mio.ModelImport import importModelFile
+import cairis.core.BorgFactory
+from cairis.core.Borg import Borg
+
+__author__ = 'Shamal Faily'
+
+
+class ModelTests(unittest.TestCase):
+
+  def setUp(self):
+    cairis.core.BorgFactory.initialise()
+    importModelFile(os.environ['CAIRIS_SRC'] + '/../examples/exemplars/NeuroGrid/NeuroGrid.xml',1,'test')
+
+  def testModelCreation(self):
+    b = Borg()  
+    b.get_dbproxy().classModel('Psychosis')
+    b.get_dbproxy().classModel('Psychosis','',True)
+    b.get_dbproxy().goalModel('Psychosis')
+    b.get_dbproxy().responsibilityModel('Psychosis')
+    b.get_dbproxy().obstacleModel('Psychosis')
+    b.get_dbproxy().taskModel('Psychosis')
+    b.get_dbproxy().assumptionPersonaModel('Claire')
+
