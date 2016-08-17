64eaada20bc72e43f7b0eeb2ebe28e5659ea6de3
Shamal Faily <shamal.faily@googlemail.com>
2016-08-17 15:38:00 +0100

    1.2.3 version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='cairis',
-      version='1.2.2',
+      version='1.2.3',
       author='Shamal Faily',
       author_email='shamal.faily@gmail.com',
       description = 'A security design tool',
