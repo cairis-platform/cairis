55ed2196714d004d0deef4a879e73f2a542a9391
Shamal Faily <shamal.faily@googlemail.com>
2016-10-10 08:56:48 +0100

    Localize jquery-contextmenu and jquery-migrate plugins

--- cairis/web/dist/js/cairis/Cairis.js
+++ cairis/web/dist/js/cairis/Cairis.js
@@ -1275,45 +1275,44 @@ function assetsDialogBox(haveEnv,callback){
         }
     });
 }
-/*
- Dialog for choosing a file
- */
+
+// Dialog for choosing a file
 function fileDialogbox(callback){
-    var dialogwindow = $("#typeOfFile");
-    var select = dialogwindow.find("select");
-    dialogwindow.dialog({
-        modal: true,
-        buttons: {
-            Ok: function () {
-                var text =  select.find("option:selected" ).text();
-                if(jQuery.isFunction(callback)){
-                    callback(text);
-                    $("#importFile").trigger('click')
-                }
-                $(this).dialog("close");
-            }
+  var dialogwindow = $("#typeOfFile");
+  var select = dialogwindow.find("select");
+  dialogwindow.dialog({
+    modal: true,
+    buttons: {
+      Ok: function () {
+        var text =  select.find("option:selected" ).text();
+        if(jQuery.isFunction(callback)){
+          callback(text);
+          $("#importFile").trigger('click')
         }
-    });
-    $(".comboboxD").css("visibility", "visible");
+        $(this).dialog("close");
+      }
+    }
+  });
+  $(".comboboxD").css("visibility", "visible");
 }
 
 function fileExportDialogbox(callback){
-    var dialogwindow = $("#typeOfFile");
-    var select = dialogwindow.find("select");
-    dialogwindow.dialog({
-        modal: true,
-        buttons: {
-            Ok: function () {
-                var text =  select.find("option:selected" ).text();
-                if(jQuery.isFunction(callback)){
-                    callback(text);
-                    $("#exportFile").trigger('click')
-                }
-                $(this).dialog("close");
-            }
+  var dialogwindow = $("#typeOfFile");
+  var select = dialogwindow.find("select");
+  dialogwindow.dialog({
+    modal: true,
+    buttons: {
+      Ok: function () {
+        var text =  select.find("option:selected" ).text();
+        if(jQuery.isFunction(callback)){
+          callback(text);
+          $("#exportFile").trigger('click')
         }
-    });
-    $(".comboboxD").css("visibility", "visible");
+        $(this).dialog("close");
+      }
+    }
+  });
+  $(".comboboxD").css("visibility", "visible");
 }
 
 /*
