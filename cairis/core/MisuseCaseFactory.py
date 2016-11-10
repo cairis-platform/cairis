03c601ba23f4894aa7f11585f52f6dd26ecdba55
Shamal Faily <shamal.faily@googlemail.com>
2016-11-10 11:56:32 +0000

    MisuseCaseFactory

---
+++ cairis/core/MisuseCaseFactory.py
@@ -0,0 +1,70 @@
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
+
+from ARM import *
+from MisuseCaseEnvironmentProperties import MisuseCaseEnvironmentProperties
+from MisuseCase import MisuseCase
+from Borg import Borg
+
+__author__ = 'Shamal Faily'
+
+def objectiveText(vulnerableAssets,threatenedAssets):
+  objectiveText = 'Exploit vulnerabilities in '
+  for idx,vulAsset in enumerate(vulnerableAssets):
+    objectiveText += vulAsset
+    if (idx != (len(vulnerableAssets) -1)):
+      objectiveText += ','
+  objectiveText += ' to threaten '
+  for idx,thrAsset in enumerate(threatenedAssets):
+    objectiveText += thrAsset
+    if (idx != (len(threatenedAssets) -1)):
+      objectiveText += ','
+  objectiveText += '.'
+  return objectiveText
+
+
+
+def build(threatName,vulnerabilityName,dbProxy = None):
+  if (dbProxy == None):
+    b = Borg()
+    dbProxy = b.dbProxy
+
+
+  envNames = dbProxy.riskEnvironments(threatName,vulnerabilityName)
+  threatId = dbProxy.getDimensionId(threatName,'threat')
+  vulId = dbProxy.getDimensionId(vulnerabilityName,'vulnerability')
+
+  envList = []
+  for envName in envNames:
+    mcEnv = MisuseCaseEnvironmentProperties(envName)
+
+    mcEnv.theRiskRating = dbProxy.riskRating(threatName,vulnerabilityName,envName)
+    envId = dbProxy.getDimensionId(envName,'environment')
+    mcEnv.theLikelihood = dbProxy.threatLikelihood(threatId,envId)
+    mcEnv.theSeverity = dbProxy.vulnerabilitySeverity(vulId,envId)
+    mcEnv.theAttackers = dbProxy.threatAttackers(threatId,envId)
+
+    threatenedAssets = dbProxy.threatenedAssets(threatId,envId)
+    vulnerableAssets = dbProxy.vulnerableAssets(vulId,envId)
+    mcEnv.theObjective = objectiveText(vulnerableAssets,threatenedAssets)
+    mcEnv.theAssets = set(threatenedAssets + vulnerableAssets)
+    envList.append(mcEnv) 
+  mc = MisuseCase(-1,'',envList,'')
+  mc.theThreatName = threatName
+  mc.theVulnerabilityName = vulnerabilityName
+  return mc
---
+++ cairis/test/test_MisuseCaseFactory.py
@@ -0,0 +1,42 @@
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
+import unittest
+import os
+from cairis.mio.ModelImport import importModelFile, importLocationsFile
+import cairis.core.BorgFactory
+from cairis.core.Borg import Borg
+import cairis.core.MisuseCaseFactory
+
+__author__ = 'Shamal Faily'
+
+
+class MisuseCaseFactoryTests(unittest.TestCase):
+
+  def setUp(self):
+    cairis.core.BorgFactory.initialise()
+    importModelFile(os.environ['CAIRIS_SRC'] + '/../examples/exemplars/NeuroGrid/NeuroGrid.xml',1,'test')
+
+  def testBuildMisuseCase(self):
+    mc = cairis.core.MisuseCaseFactory.build('Social Engineering','Certificate ubiquity')
+    mcEnv = mc.theEnvironmentProperties[0]
+    self.assertEqual(mcEnv.theEnvironmentName,'Psychosis')
+    self.assertEqual(mcEnv.theLikelihood,'Occasional')
+    self.assertEqual(mcEnv.theSeverity,'Critical')
+    self.assertEqual(mcEnv.theRiskRating,'Undesirable')
+    self.assertEqual(mcEnv.theObjective,'Exploit vulnerabilities in User certificate to threaten User certificate,Client workstation.')
+
