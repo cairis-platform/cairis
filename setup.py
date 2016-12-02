abbacac4ab6a4b926a2c7f673a0ff8c0f6114805
cairisuser <cairisuser@germaneriposte.org>
2016-12-02 08:38:34 +0000

    Increment CAIRIS version

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='cairis',
-      version='1.2.10',
+      version='1.2.11',
       author='Shamal Faily',
       author_email='shamal.faily@gmail.com',
       description = 'A security design tool',
       license = 'Apache Software License',
       url='https://github.com/failys/cairis',
-      download_url='https://github.com/failys/cairis/tarball/1.2.10',
+      download_url='https://github.com/failys/cairis/tarball/1.2.11',
       packages=['cairis'],
       include_package_data=True,
       data_files = [('cairis/examples', egFiles)],
