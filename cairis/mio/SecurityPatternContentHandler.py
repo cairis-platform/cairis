116cd5ba9ba8e00821a84be0453c51adea6df7f4
Shamal Faily <cairisuser@germaneriposte.org>
2016-12-14 08:30:42 +0000

    Fix broken security patterns test

--- cairis/mio/SecurityPatternContentHandler.py
+++ cairis/mio/SecurityPatternContentHandler.py
@@ -236,8 +236,8 @@ class SecurityPatternContentHandler(ContentHandler,EntityResolver):
         spRationale = sp[2]
         if spName in spDict:
           spDict[spName] = spValue
-        if srName in srDict:
-          srDict[srName] = spRationale
+        if spName in srDict:
+          srDict[spName] = spRationale
       spValues = []
       spValues.append(spDict['confidentiality'])
       spValues.append(spDict['integrity'])
