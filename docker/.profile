86601347ea016f4a3f90b6942093d63e91de5f74
Shamal Faily <cairisuser@germaneriposte.org>
2017-01-17 20:56:11 +0000

    Fix broken Docker changes

--- docker/.profile
+++
@@ -1,5 +0,0 @@
-mysql -h localhost -u root <<!
-set global max_sp_recursion_depth = 255;
-flush tables;
-flush privileges;
-!
--- docker/cmd.sh
+++ docker/cmd.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
-. .profile
 service mysql start
+mysql -h localhost -u root <<!
+set global max_sp_recursion_depth = 255;
+flush tables;
+flush privileges;
+!
 /cairis/cairis/bin/cairisd.py runserver
