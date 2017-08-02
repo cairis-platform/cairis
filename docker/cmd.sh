0b118129b7dde46e2c01636804c17aeafd49e51b
Shamal Faily <sfaily@bournemouth.ac.uk>
2017-08-02 12:57:34 +0100

    Use linked containers

--- docker/cmd.sh
+++ docker/cmd.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-set -e
-service mysql start
-mysql -h localhost -u root <<!
-set global max_sp_recursion_depth = 255;
-flush tables;
-flush privileges;
-!
 mod_wsgi-express start-server /cairis/cairis/bin/cairis.wsgi --user www-data --group www-data
--- docker/setupDb.sh
+++ docker/setupDb.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-service mysql start
-/cairis/cairis/test/initdb.sh
+set -x
+mysql -h cairis-mysql --user=root --password='my-secret-pw' < $CAIRIS_SRC/test/createdb.sql
+mysql -h cairis-mysql --user=cairisuser --password='cairisuser' --database=cairis_default < $CAIRIS_SRC/sql/init.sql
+mysql -h cairit-mysql --user=cairisuser --password='cairisuser' --database=cairis_default < $CAIRIS_SRC/sql/procs.sql
 /cairis/cairis/bin/add_cairis_user.py test test
-mysql -h localhost -u root <<!
+mysql -h cairis-mysql --user=root --password=my-secret-pw <<!
 set global max_sp_recursion_depth = 255;
 flush tables;
 flush privileges;
 !
+mod_wsgi-express start-server /cairis/cairis/bin/cairis.wsgi --user www-data --group www-data
