a1955f0fed9ca6e5912e7f4a66297e7e6e4dbd6b
Shamal Faily <sfaily@bournemouth.ac.uk>
2017-08-02 23:54:27 +0100

    Updates for docker

--- docker/setupDb.sh
+++ docker/setupDb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
-mysql -h cairis-mysql --user=root --password='my-secret-pw' < createdb.sql
-mysql -h cairis-mysql --user=cairisuser --password='cairisuser' --database=cairis_default < $CAIRIS_SRC/sql/init.sql
-mysql -h cairis-mysql --user=cairisuser --password='cairisuser' --database=cairis_default < $CAIRIS_SRC/sql/procs.sql
+mysql -h cairis-mysql --user=root --password=my-secret-pw < createdb.sql
+mysql -h cairis-mysql --user=cairisuser --password=cairisuser --database=cairis_default < $CAIRIS_SRC/sql/init.sql
+mysql -h cairis-mysql --user=cairisuser --password=cairisuser --database=cairis_default < $CAIRIS_SRC/sql/procs.sql
 /cairis/cairis/bin/add_cairis_user.py test test
 mysql -h cairis-mysql --user=root --password=my-secret-pw <<!
 set global max_sp_recursion_depth = 255;
