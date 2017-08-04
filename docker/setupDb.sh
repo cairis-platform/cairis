fa9996aa651e62d1f8b1c930ab9b7911f4040b10
Shamal Faily <shamal.faily@googlemail.com>
2017-08-04 11:19:25 +0100

    Updates for Docker

--- docker/setupDb.sh
+++ docker/setupDb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-mysql -h cairis-mysql --user=root --password=my-secret-pw < createdb.sql
+mysql -h cairis-mysql --user=root < createdb.sql
 mysql -h cairis-mysql --user=cairisuser --password=cairisuser --database=cairis_default < $CAIRIS_SRC/sql/init.sql
 mysql -h cairis-mysql --user=cairisuser --password=cairisuser --database=cairis_default < $CAIRIS_SRC/sql/procs.sql
 /cairis/cairis/bin/add_cairis_user.py test test
