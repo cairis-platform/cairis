3bf7c331e41e59ea3af027444947e6ce6c72a964
Shamal Faily <shamal.faily@googlemail.com>
2017-08-07 13:49:01 +0100

    Consistent live demo script

--- docker/buildDemo.sh
+++ docker/buildDemo.sh
@@ -3,15 +3,10 @@ set -x
 
 export REPOS_DIR=/tmp
 export CAIRIS_REPO=$REPOS_DIR/cairis
-export WEBINOS_DESIGN_DATA_REPO=$REPOS_DIR/webinos-design-data
-
-sudo pip install --upgrade pydot certifi openpyxl requests jsonpickle argparse urllib uno
 
 rm -rf $CAIRIS_REPO
-rm -rf $WEBINOS_DESIGN_DATA_REPO
 
 git clone http://github.com/failys/cairis $CAIRIS_REPO
-git clone http://github.com/webinos/webinos-design-data $WEBINOS_DESIGN_DATA_REPO
 
 sudo docker stop CAIRIS
 sudo docker stop cairis-mysql
@@ -20,6 +15,19 @@ sudo docker rmi $(sudo docker images -q)
 sudo docker run --name cairis-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:5.5
 sudo docker run --name CAIRIS --link cairis-mysql:mysql -d -P -p 80:8000 --net=bridge shamalfaily/cairis
 sleep 60
-$CAIRIS_REPO/cairis/bin/model_import_web.py --url http://localhost --database ACME_Water --image_dir $CAIRIS_REPO/examples/exemplars/ACME_Water --rich_pic stcsContext.jpg $CAIRIS_REPO/examples/exemplars/ACME_Water/ACME_Water.xml
+
 $CAIRIS_REPO/cairis/bin/model_import_web.py --url http://localhost --database NeuroGrid --image_dir $CAIRIS_REPO/examples/exemplars/NeuroGrid --rich_pic NeuroGridContext.jpg $CAIRIS_REPO/examples/exemplars/NeuroGrid/NeuroGrid.xml
-$WEBINOS_DESIGN_DATA_REPO/scripts/regenerate_webservices.sh
+$CAIRIS_REPO/cairis/bin/model_import_web.py --url http://localhost --database ACME_Water --image_dir $CAIRIS_REPO/examples/exemplars/ACME_Water --rich_pic stcsContext.jpg $CAIRIS_REPO/examples/exemplars/ACME_Water/ACME_Water.xml
+
+# Point the test session to NeuroGrid by default
+curl -X POST http://localhost/api/settings/database/NeuroGrid/open?session_id=test
+
+# Uncomment below lines if you want to add webinos to the live demo
+#rm -rf $WEBINOS_DESIGN_DATA_REPO
+#export WEBINOS_DESIGN_DATA_REPO=$REPOS_DIR/webinos-design-data
+#git clone http://github.com/webinos/webinos-design-data $WEBINOS_DESIGN_DATA_REPO
+
+# The regeneration script uses Python converts spreadsheets and dot files to CAIRIS, but you need to install some pre-requisite packages.  These install fine on Linux, but are problematic to install on Mac OS X.  I haven't tested the scripts on Windows
+# sudo pip install --upgrade pydot certifi openpyxl requests jsonpickle argparse urllib uno
+
+#$WEBINOS_DESIGN_DATA_REPO/scripts/regenerate_webservices.sh
