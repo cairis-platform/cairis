b39ef7b64abdb901185f6aa060c12b5f33c0ac82
Shamal Faily <shamal.faily@gmail.com>
2017-11-22 11:39:17 -0800

    Fix tests

--- travisConfig/createdb.sql
+++ travisConfig/createdb.sql
@@ -21,9 +21,7 @@
 drop database if exists `cairis_user`;
 create database if not exists `cairis_user`;
 
-drop user if exists `cairis_test`;
-create user if not exists 'cairis_test'@'%' identified by 'cairis_test';
-grant usage on *.* to 'cairis_test'@'%' with max_queries_per_hour 0 max_connections_per_hour 0 max_updates_per_hour 0 max_user_connections 0;
+grant usage on *.* to 'cairis_test'@'%' identified by 'cairis_test' with max_queries_per_hour 0 max_connections_per_hour 0 max_updates_per_hour 0 max_user_connections 0;
 flush privileges;
 
 drop database if exists `cairis_test`;
