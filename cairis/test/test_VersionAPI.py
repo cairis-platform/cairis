f8aeca27aaeef8db9dabd75f630d5b122853bc2c
cairisuser <Shamal Faily>
2018-06-10 12:05:50 -0700

    1.6.7 release

--- cairis/test/test_VersionAPI.py
+++ cairis/test/test_VersionAPI.py
@@ -51,4 +51,4 @@ class VersionAPITests(CairisDaemonTestCase):
       responseData = rv.data
     objts = jsonpickle.decode(responseData)
     self.assertIsNotNone(objts, 'No results after deserialization')
-    self.assertEqual(objts,'1.6.6')
+    self.assertEqual(objts,'1.6.7')
