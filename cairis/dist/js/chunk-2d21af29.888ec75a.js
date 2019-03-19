2c92fef53d2e35dcb4bcac8b56bede87951010fd
Shamal Faily <shamal.faily@gmail.com>
2019-03-19 06:44:22 +0000

    Updated UI

---
+++ cairis/dist/js/chunk-2d21af29.888ec75a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21af29"],{be4d:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("p",[n._v("404 Not Found")])},o=[],a={name:"NotFound"},c=a,l=t("2877"),r=Object(l["a"])(c,u,o,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d21af29.888ec75a.js.map
