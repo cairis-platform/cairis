df92dd2ee1ee1b34d929c34becd7fe94edd4c820
Shamal Faily <shamal.faily@gmail.com>
2019-04-28 10:16:10 -0700

    Add missing curl package

--- cairis/bin/installUI.sh
+++ cairis/bin/installUI.sh
@@ -20,6 +20,7 @@
 
 export UI_REPO=/tmp/cairis-ui
 rm -rf $UI_REPO
+apt-get install curl
 curl -sL https://deb.nodesource.com/setup_10.x | bash - && apt-get install -y nodejs
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
