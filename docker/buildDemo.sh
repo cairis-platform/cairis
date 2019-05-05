9d57143c4e7f74bee889fdd13a104454bec19814
Shamal Faily <shamal.faily@gmail.com>
2019-05-05 12:21:02 -0700

    Update dependencies

--- docker/buildDemo.sh
+++ docker/buildDemo.sh
@@ -22,7 +22,7 @@ sleep 60
 # Make sure the requests and argparse packages are installed before running model_import_web.py or web_cimport.py.
 $CAIRIS_REPO/cairis/bin/model_import_web.py --url http://localhost --database NeuroGrid $CAIRIS_REPO/examples/exemplars/NeuroGrid.cairis
 $CAIRIS_REPO/cairis/bin/web_cimport.py --url http://localhost --database NeuroGrid --type locations $CAIRIS_REPO/examples/exemplars/NeuroGrid/ComLab.xml
-$CAIRIS_REPO/cairis/bin/model_import_web.py --url http://localhost --database ACME_Water $CAIRIS_REPO/examples/exemplars/ACME_Water/ACME_Water.cairis
+$CAIRIS_REPO/cairis/bin/model_import_web.py --url http://localhost --database ACME_Water $CAIRIS_REPO/examples/exemplars/ACME_Water.cairis
 $CAIRIS_REPO/cairis/bin/web_cimport.py --url http://localhost --database ACME_Water --type locations $CAIRIS_REPO/examples/exemplars/ACME_Water/PooleWWTW.xml
 
 # Uncomment below lines if you want to add webinos to the live demo
