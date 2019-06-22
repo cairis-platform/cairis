dbc76ae8c89c11182817a3489d1b1313d39618bf
Shamal Faily <sfaily@CargoCult.local>
2019-06-22 22:01:45 +0100

    Use latest Ubuntu docker image

--- docker/setupDb.sh
+++ docker/setupDb.sh
@@ -3,7 +3,6 @@ set -x
 mysql -h cairis-mysql --user=root --password=my-secret-pw < createdb.sql
 mysql -h cairis-mysql --user=cairis_test --password=cairis_test --database=cairis_test_default < $CAIRIS_SRC/sql/init.sql
 mysql -h cairis-mysql --user=cairis_test --password=cairis_test --database=cairis_test_default < $CAIRIS_SRC/sql/procs.sql
-/cairis/cairis/bin/add_cairis_user.py test test Test
 mysql -h cairis-mysql --user=root --password=my-secret-pw <<!
 set global max_sp_recursion_depth = 255;
 flush tables;
