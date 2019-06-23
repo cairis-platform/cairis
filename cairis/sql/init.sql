08b8e75b0c95bc1ecb9a9f45d385c041303d52f0
Shamal Faily <shamal.faily@gmail.com>
2019-06-23 14:38:35 -0700

    2.1 release

--- cairis/sql/init.sql
+++ cairis/sql/init.sql
@@ -4008,7 +4008,7 @@ CREATE VIEW personal_risk as
 
 
 
-INSERT INTO version (major,minor,patch) VALUES (2,0,2);
+INSERT INTO version (major,minor,patch) VALUES (2,1,0);
 INSERT INTO attributes (id,name) VALUES (103,'did');
 INSERT INTO trace_dimension values (0,'requirement');
 INSERT INTO trace_dimension values (1,'persona');
