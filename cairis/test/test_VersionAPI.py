e5e9b59d417307e9635049fbcc785eab10e577c6
Shamal Faily <shamal.faily@gmail.com>
2019-07-11 08:04:31 -0700

    2.1.1. release

--- cairis/test/test_VersionAPI.py
+++ cairis/test/test_VersionAPI.py
@@ -51,4 +51,4 @@ class VersionAPITests(CairisDaemonTestCase):
       responseData = rv.data
     objts = jsonpickle.decode(responseData)
     self.assertIsNotNone(objts, 'No results after deserialization')
-    self.assertEqual(objts,'2.1.0')
+    self.assertEqual(objts,'2.1.1')
