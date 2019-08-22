78c7e0fb74ec49fd9c81d8ae89c9059c9bc67310
Shamal Faily <sfaily@CargoCult.local>
2019-08-22 21:36:28 +0100

    quickInstall script

---
+++ quickInstall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+CAIRIS_ROOT=$1
+ROOTPW=$2
+
+sudo rm -rf $CAIRIS_ROOT
+
+git clone https://github.com/cairis-platform/cairis $CAIRIS_ROOT
+
+sudo apt-get install python3-dev build-essential mysql-server mysql-client graphviz docbook dblatex python3-pip python3-numpy git libmysqlclient-dev --no-install-recommends texlive-latex-extra docbook-utils inkscape libxml2-dev libxslt1-dev poppler-utils python3-setuptools pandoc
+
+sudo pip3 install -r $CAIRIS_ROOT/requirements.txt
+
+$CAIRIS_ROOT/cairis/bin/quick_setup_headless.py --rootDir=$CAIRIS_ROOT/cairis --dbRootPassword=$ROOTPW --logLevel=debug 
+
+
+CMD1='flush privileges; use mysql; update user set authentication_string=PASSWORD("'
+CMD2='") where User="root"; update user set plugin="mysql_native_password" where User="root";'
+CMD="$CMD1$ROOTPW$CMD2"
+
+sudo service mysql stop
+sudo mkdir -p /var/run/mysqld
+sudo chown mysql:mysql /var/run/mysqld
+sudo /usr/bin/mysqld_safe --skip-grant-tables &
+sleep 5
+mysql -u root --execute="$CMD"
+sudo pkill mysqld_safe
+sudo pkill mysqld
+sudo service mysql start
+
+source $HOME/.bashrc
+
+sudo -E $CAIRIS_ROOT/cairis/bin/installUI.sh
