3e5841cc2a7cebfcf7e5149728b0655521746b71
Shamal Faily <sfaily@CargoCult.local>
2019-10-30 08:03:10 +0000

    Cron-friendly rebuildServer.sh (Issue #470)

--- rebuildServer.sh
+++ rebuildServer.sh
@@ -16,8 +16,12 @@
 #  specific language governing permissions and limitations
 #  under the License.
 
-CAIRIS_ROOT=$HOME/cairis
+# Replace CAIRIS_ACCOUNT, CAIRIS_HOME, and CAIRIS_ROOT based on the account running CAIRIS
+CAIRIS_ACCOUNT=sfaily
+CAIRIS_HOME=/home/sfaily
+CAIRIS_ROOT=/home/sfaily/cairis
 export CAIRIS_SRC=$CAIRIS_ROOT/cairis
+
 ROOTPW=$1
 MAILSVR=$2
 MAILPORT=$3
@@ -43,9 +47,9 @@ sudo pip3 install -r $CAIRIS_ROOT/requirements.txt
 sudo pip3 install -r $CAIRIS_ROOT/wsgi_requirements.txt
 
 export PYTHONPATH=$CAIRIS_ROOT
-$CAIRIS_ROOT/cairis/bin/server_setup_headless.py --rootDir=$CAIRIS_ROOT/cairis --webPort=8000 --dbRootPassword=$ROOTPW --logLevel=debug --mailServer=$MAILSVR --mailPort=$MAILPORT --mailUser=$MAILUSER --mailPasswd=$MAILPASSWD
+$CAIRIS_ROOT/cairis/bin/server_setup_headless.py --rootDir=$CAIRIS_ROOT/cairis --configFile=$CAIRIS_HOME/cairis.cnf --webPort=8000 --dbRootPassword=$ROOTPW --logLevel=debug --mailServer=$MAILSVR --mailPort=$MAILPORT --mailUser=$MAILUSER --mailPasswd=$MAILPASSWD
 
-SVCFILE="[Unit]\nDescription=cairisd\n\n[Service]\nUser=$USER\nWorkingDirectory=$CAIRIS_ROOT\nEnvironment=\"CAIRIS_CFG=$HOME/cairis.cnf\"\nEnvironment=\"PYTHONPATH=\${PYTHONPATH}:$CAIRIS_ROOT\"\nExecStart=mod_wsgi-express start-server $CAIRIS_ROOT/cairis/bin/cairis.wsgi --user www-data --group www-data\nRestart=on-failure\n\n[Install]\nWantedBy=multi-user.target"
+SVCFILE="[Unit]\nDescription=cairisd\n\n[Service]\nUser=$CAIRIS_ACCOUNT\nWorkingDirectory=$CAIRIS_ROOT\nEnvironment=\"CAIRIS_CFG=$CAIRIS_HOME/cairis.cnf\"\nEnvironment=\"PYTHONPATH=\${PYTHONPATH}:$CAIRIS_ROOT\"\nExecStart=mod_wsgi-express start-server $CAIRIS_ROOT/cairis/bin/cairis.wsgi --user www-data --group www-data\nRestart=on-failure\n\n[Install]\nWantedBy=multi-user.target"
 echo -e $SVCFILE | sudo tee /etc/systemd/system/cairis.service
 
 sudo systemctl enable /etc/systemd/system/cairis.service
