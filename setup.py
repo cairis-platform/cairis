25e089013c757f06d60ca8b5ef99987994512cf8
Shamal Faily <shamal.faily@gmail.com>
2019-12-24 00:31:03 +0000

    2.3.0 release2.3.0 release

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='cairis',
-      version='2.2.4',
+      version='2.3.0',
       author='Shamal Faily',
       author_email='shamal.faily@gmail.com',
       description = 'A security design tool',
       license = 'Apache Software License',
       url='https://github.com/cairis-platform/cairis',
-      download_url='https://github.com/cairis-platform/cairis/tarball/2.2.4',
+      download_url='https://github.com/cairis-platform/cairis/tarball/2.3.0',
       packages=['cairis'],
       include_package_data=True,
       data_files = [('cairis/examples', egFiles)],
