9fd31bff39ca2470e39e7da725aa44527919fc54
Shamal Faily <shamal.faily@gmail.com>
2020-01-04 21:49:26 +0000

    Fix #497

--- cairis/bin/reset_cairis_user.py
+++ cairis/bin/reset_cairis_user.py
@@ -16,27 +16,54 @@
 #  specific language governing permissions and limitations
 #  under the License.
 
+import logging
 import argparse
 import sys
 from cairis.core.Borg import Borg
 import cairis.core.BorgFactory
-from cairis.core.dba import resetUser,resetUsers
+from cairis.core.dba import resetUser,accounts
+from cairis.mio.ModelExport import exportPackage
+from cairis.bin.cimport import package_import
 
 __author__ = 'Shamal Faily'
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('reset_user')
+
+
+def resetDatabase(cairisRoot,rPasswd,dbHost,dbPort,user,isReload):
+  cairis.core.BorgFactory.initialise(user=user,db='default')
+  b = Borg()
+
+  packageFile = '/tmp/' + user + '.cairis'
+  if (isReload == '1'):
+    logger.info('Exporting ' + user + ' default database')
+    exportPackage(packageFile,b.dbProxy)
+
+  logger.info('Resetting ' + user)
+  resetUser(cairisRoot,rPasswd, dbHost, dbPort, user)
+
+  cairis.core.BorgFactory.initialise(user=user,db='default')
+  if (isReload == '1'):
+    logger.info('Re-importing ' + user + ' default database')
+    pkgStr = open(packageFile,'rb').read()
+    package_import(pkgStr)
 
 def main():
   parser = argparse.ArgumentParser(description='Computer Aided Integration of Requirements and Information Security - Reset CAIRIS user')
   parser.add_argument('user',help='Email address or all for all users')
+  parser.add_argument('--reload',dest='isReload',help='If 1 is set, reload the contents of the default database', default='0')
+
   args = parser.parse_args()
 
   cairis.core.BorgFactory.dInitialise()
   b = Borg()
   
   if (args.user != 'all'):
-    resetUser(b.cairisRoot,b.rPasswd, b.dbHost, b.dbPort, args.user)
+    resetDatabase(b.cairisRoot,b.rPasswd,b.dbHost,b.dbPort,args.user,args.isReload)
   else:
-    resetUsers(b.cairisRoot,b.rPasswd, b.dbHost, b.dbPort)
+    for email in accounts(b.cairisRoot,b.dbHost,b.dbPort):
+      resetDatabase(b.cairisRoot,b.rPasswd,b.dbHost,b.dbPort,email,args.isReload)
 
 if __name__ == '__main__':
   try:
