024b79507a9a3a6b1d2a514a671fe817a049b36f
Shamal Faily <shamal.faily@gmail.com>
2020-04-24 16:27:07 +0100

    2.3.3. release

--- cairis/sql/init.sql
+++ cairis/sql/init.sql
@@ -4164,7 +4164,7 @@ CREATE VIEW conceptMapModel_all as
 
 
 
-INSERT INTO version (major,minor,patch) VALUES (2,3,2);
+INSERT INTO version (major,minor,patch) VALUES (2,3,3);
 INSERT INTO attributes (id,name) VALUES (103,'did');
 INSERT INTO trace_dimension values (0,'requirement');
 INSERT INTO trace_dimension values (1,'persona');
