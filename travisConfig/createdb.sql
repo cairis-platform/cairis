54483590713d020808198f7740471ab8e2fd32b5
Shamal Faily <shamal.faily@gmail.com>
2020-11-03 04:19:09 -0800

    Fix Travis mysql problems

--- travisConfig/createdb.sql
+++ travisConfig/createdb.sql
@@ -17,7 +17,6 @@
   under the License.
 */
 
-
 drop database if exists `cairis_user`;
 create database if not exists `cairis_user`;
 
@@ -25,15 +24,18 @@ drop database if exists `cairis_owner`;
 create database if not exists `cairis_owner`;
 create table cairis_owner.db_owner(db varchar(64), owner varchar(32), primary key(db,owner)) engine=innodb;
 
+drop user if exists cairis_test;
+create user if not exists 'cairis_test'@'%' identified by 'cairis_test';
+grant usage on `cairis_test`.* to 'cairis_test'@'%';
+flush privileges;
+
 drop database if exists `cairis_test_default`;
 create database if not exists `cairis_test_default`;
-
-create user cairis_test identified by 'cairis_test';
 grant all privileges on `cairis_test_default`.* to 'cairis_test'@'%';
 set global max_sp_recursion_depth = 255;
 
 alter database cairis_test_default default character set utf8mb4;
-alter database cairis_test_default default collate utf8mb4_general_ci;
+alter database cairis_test_default default collate utf8mb4_0900_ai_ci;
 
 flush tables;
 flush privileges;
