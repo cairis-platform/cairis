062113ec3b4b843883baf41be2352269f68fc2a9
Shamal Faily <shamal.faily@gmail.com>
2020-11-17 11:32:58 +0000

    Fix #585

--- docker/createdb.sql
+++ docker/createdb.sql
@@ -7,9 +7,10 @@ create table cairis_owner.db_owner(db varchar(64), owner varchar(32), primary ke
 
 drop user if exists cairis_test;
 create user if not exists 'cairis_test'@'%' identified by 'cairis_test';
-grant usage on `cairis_test`.* to 'cairis_test'@'%' with max_queries_per_hour 0 max_connections_per_hour 0 max_updates_per_hour 0 max_user_connections 0;
+grant usage on `cairis_test`.* to 'cairis_test'@'%';
 flush privileges;
 
+drop database if exists `cairis_test_default`;
 create database if not exists `cairis_test_default`;
 grant all privileges on `cairis_test_default`.* to 'cairis_test'@'%';
 set global max_sp_recursion_depth = 255;
