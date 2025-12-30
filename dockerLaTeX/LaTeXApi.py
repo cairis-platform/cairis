10013fbec2b94d230a01f099dad17f6de67a7ccf
Shamal Faily <sfaily@dstl.gov.uk>
2025-12-30 15:28:24 +0000

    Tidy up LaTeXApi.py

--- dockerLaTeX/LaTeXApi.py
+++ dockerLaTeX/LaTeXApi.py
@@ -1,21 +1,18 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/python
 from flask import Flask, request, abort
-from armid import *
 import os
-
 
 app = Flask(__name__)
 
-
 @app.route('/latexApi', methods=['POST'])
 def index():
-    try:
-        dockBookCmd = request.values.get('docBookCmd')
-        os.system(dockBookCmd)
-        return "Success"
-    except:
-        abort(500)
+  try:
+    dockBookCmd = request.values.get('docBookCmd')
+    os.system(dockBookCmd)
+    return "Success"
+  except:
+    abort(500)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+  app.run(host="0.0.0.0", debug=True)
