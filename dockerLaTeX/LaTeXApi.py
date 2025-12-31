53c1faec8c4b19788a9bccd3286b7359c9e665f6
Shamal Faily <sfaily@dstl.gov.uk>
2025-12-31 17:56:11 +0000

    Working

--- dockerLaTeX/LaTeXApi.py
+++ dockerLaTeX/LaTeXApi.py
@@ -7,20 +7,12 @@ app = Flask(__name__)
 
 @app.route('/latexApi', methods=['POST'])
 def index():
-  allowedCommands = set(['docbook2html','docbook2rtf','dblatex','pandoc'])
   try:
     dockBookCmd = request.values.get('docBookCmd')
-    if not docBookCmd:
-      print("No DocBook command")
-      abort(400)
-    cmd = docBookCmd.split(' ')[0]
-    if (docBookCmd.split(' ')[0] not in allowedCommands):
-      print(cmd + " is not allowed.")
-      abort(400)
     os.system(dockBookCmd)
     return "Success"
   except Exception:
     abort(500)
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0",debug=True)
+  app.run(host="0.0.0.0")
